a4fc8f8775ff85ddcd53a1105ce07a6ec504c85c
Jerónimo Mendes <jeronimo.m.mendes@gmail.com>
2022-08-22 18:28:45 +0100

    Start Swift project

---
+++ BatterySaver/BatterySaverApp.swift
@@ -0,0 +1,17 @@
+//
+//  BatterySaverApp.swift
+//  BatterySaver
+//
+//  Created by Jerónimo Mendes on 22/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct BatterySaverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
